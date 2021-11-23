ec8c3136a0fd13143f4ede7984e8ef6e1706c5d9
vortexcyba <88479442+vortexcyba@users.noreply.github.com>
2021-11-23 17:40:51 +0530

    Update web_routes.py

--- tgfilestream/web_routes.py
+++ tgfilestream/web_routes.py
@@ -29,12 +29,12 @@ routes = web.RouteTableDef()
 ongoing_requests: Dict[str, int] = defaultdict(lambda: 0)
 
 
-@routes.head(r"/stream/{id:\d+}")
+@routes.head(r"/file/:{chat_id}/:{message_id}")
 async def handle_head_request(req: web.Request) -> web.Response:
     return await handle_request(req, head=True)
 
 
-@routes.get(r"/stream/{id:\d+}")
+@routes.get(r"/file/:{chat_id}/:{message_id}")
 async def handle_get_request(req: web.Request) -> web.Response:
     return await handle_request(req, head=False)
 
